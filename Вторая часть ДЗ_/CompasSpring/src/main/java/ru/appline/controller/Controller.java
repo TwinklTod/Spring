d4eb938a0504b469562598b0f66ec7ebd576fcc3
EugenePC <skornevgen@gmail.com>
2024-01-29 09:43:22 +0500

    Вторая часть ДЗ:

---
+++ Вторая часть ДЗ_/CompasSpring/src/main/java/ru/appline/controller/Controller.java
@@ -0,0 +1,34 @@
+package ru.appline.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class Controller {
+
+    private Map<String, String> ranges = new HashMap<>();
+
+    @PostMapping("/ranges")
+    public void setRanges(@RequestBody Map<String, String> ranges) {
+        this.ranges = ranges;
+    }
+
+    @GetMapping("/side")
+    public String getSide(@RequestParam int degree) {
+        if (ranges.isEmpty()) {
+            return "Ошибка: Диапазоны не заданы.";
+        }
+        for (Map.Entry<String, String> entry : ranges.entrySet()) {
+            String[] range = entry.getValue().split("-");
+            int start = Integer.parseInt(range[0]);
+            int end = Integer.parseInt(range[1]);
+            if (degree >= start && degree <= end) {
+                return entry.getKey();
+            }
+        }
+        return "Unknown";
+    }
+}
+
